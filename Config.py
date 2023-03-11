d58e3a938896a8ad6969596b72eb84f1865f2e33
xious1 <127556239+xious1@users.noreply.github.com>
2023-03-12 01:27:05 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,11 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "25656584"))
     API_HASH = os.environ.get("API_HASH", "df0659031ea6af4d4e8f905ad0c09750")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6085468201:AAHUneVmxENO1qmA5fsMfbkVaLPQJ3YYHZw")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOGYBu0yGADpbhhxAyS8_MZnadCXXrcoBEWxyMU66FyAkwsH5tI4A7SL8sT9o6-_NBh3EY2_E7L5dH30HQJ6JcApjg5bGsPPFXTvLxq1zV1cyHh5jKtDK5HnlEOgNlgkwwku3W2OeuxmfF704liE2whbns3eljMxf1dJlVsbfERkyH2tFc9aIaicn2jydJo3nDVOi37eJCzVkvsLgdbQcf7kBCxBENV8UJUTudN5T074uMYzCPvelFg2qU48bl6TKQ3P7If2L574TNvTP0zx4m55G1L89oy_bI5Upk8RNnC90eWAJHE8Y3DSrc1HR0TIv4eWVFxjdIXhfyIdfloU_2K8kL6E= ")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "String
+
+1BVtsOGYBuzt-22pvzm_wZ5Vv7v7EQehnhdH2BilMk49KNLVXBgIxGbs6Fmnn9BWGOWq01aEHAbdxYy7KcyIaLHEjQJi4K4XOMJ97iW5Hf-UO3A0LWJ6MzsGsbmBm531L06EvcrEqR8bpWGB6bADShPCXoJGJjsiRmjK2Ffm6h1lFVdI_TkIV8RfSnVS9nY11fWmrfpBkCGH2vF6u7ed3Z2l-yHtzS4En_Em1XK03Erd-2D1Vk9iypiaYhzQRBHSqYj-KgHmzPnvsVFgQFZVQUPGp7npELaKPAqydOjuw5Y3-WDXinoaNdq3Teoq4ivkAzZBG9n_dzNVSpOdYUdb6nc1jnrZOg84= 
+
+")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", True)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "ShelbysMusicbot")
